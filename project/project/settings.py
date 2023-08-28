7cd2fc27b872e7127c0fa8fadd8c92dbd8e9329d
Megan <megantech@hotmail.com>
2023-08-28 15:24:08 +0000

    Added proxy into package.json file and set up axiosDefaults.js with reminder regarding no need for seperate baseURL

--- project/project/settings.py
+++ project/project/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
-import re
+# import re
 if os.path.exists('env.py'):
     import env
 
@@ -63,8 +63,7 @@ DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = [
     os.environ.get('ALLOWED_HOST'),
-    '3000-megwana-apidjango-dajv8bjqyqq.ws-eu104.gitpod.io',
-    '8000-megwana-apidjango-dajv8bjqyqq.ws-eu104.gitpod.io'
+    'localhost',
 ]
 
 CSRF_TRUSTED_ORIGINS = [
@@ -113,11 +112,14 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
 ]
 
-if 'CLIENT_ORIGIN_DEV' in os.environ:
-    extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
-    CORS_ALLOWED_ORIGIN_REGEXES = [
-        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
-    ]
+CORS_ALLOWED_ORIGINS = [
+    os.environ.get('CLIENT_ORIGIN')
+]
+# if 'CLIENT_ORIGIN_DEV' in os.environ:
+#     extracted_url = re.match(r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE).group(0)
+#     CORS_ALLOWED_ORIGIN_REGEXES = [
+#         rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
+#     ]
 
 CORS_ALLOW_CREDENTIALS = True
 
